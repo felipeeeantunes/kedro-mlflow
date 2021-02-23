0bd3820b428e04fcb01572ace06df8f9e6453003
Galileo Galilei <yolan.honore.rouge@gmail.com>
2021-02-23 23:48:22 +0100

    finish to remove no longer necessary utils

--- kedro_mlflow/utils.py
+++ kedro_mlflow/utils.py
@@ -1,20 +1,8 @@
 from pathlib import Path
-from typing import Union
+from typing import List, Union
 
 
-def _validate_project_path(project_path: Union[str, Path, None] = None) -> Path:
-    project_path = Path(project_path or Path.cwd())
-    return project_path
-
-
-def _already_updated(project_path: Union[str, Path, None] = None) -> bool:
-    project_path = _validate_project_path(project_path)
-    # TODO: add a better check
-    flag = (project_path / "conf" / "base" / "mlflow.yml").is_file()
-    return flag
-
-
-def _parse_requirements(path, encoding="utf-8"):
+def _parse_requirements(path: Union[str, Path], encoding="utf-8") -> List:
     with open(path, mode="r", encoding=encoding) as file_handler:
         requirements = [
             x.strip() for x in file_handler if x.strip() and not x.startswith("-r")
